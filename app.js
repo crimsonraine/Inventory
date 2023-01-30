0875e37ed3e7b255be09416a42a1e3646b860f18
Kimberley Ni <wukimberley98@gmail.com>
2023-01-30 12:19:49 -0500

    up to 4.2.2.2

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const db = require('./db/db_connection');
 const logger = require("morgan");
 
 //set up the server
@@ -17,14 +18,38 @@ app.get( "/", ( req, res ) => {
 } );
 
 // define a route for the stuff inventory page
+const read_stuff_all_sql = `
+    SELECT 
+        id, core, wood, length, flexibility
+    FROM
+        wands
+`
 app.get( "/inventory", ( req, res ) => {
-    res.sendFile( __dirname + "/views/inventory.html" );
-} );
+    db.execute(read_stuff_all_sql, (error, results) => {
+        if (error)
+            res.status(500).send(error); //Internal Server Error
+        else
+            res.send(results);
+    });
+});
 
 // define a route for the item detail page
-app.get( "/inventory/det", ( req, res ) => {
-    res.sendFile( __dirname + "/views/det.html" );
-} );
+const read_item_sql = `
+    SELECT 
+    core, wood, length, flexibility
+    FROM
+        wands
+    WHERE
+        id = ?
+`
+app.get( "/inventory/det/:id", ( req, res, next ) => {
+    db.execute(read_item_sql, [req.params.id], (error, results) => {
+        if (error)
+            res.status(500).send(error); //Internal Server Error
+        else
+            res.send(results[0]); // results is still an array
+    });
+});
 
 // start the server
 app.listen( port, () => {
